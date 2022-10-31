135e1c8e1dd42bed324b4ebc2a0ca082ec1ba27b
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-31 19:26:50 +0800

    first commit

---
+++ src/LowDB.ts
@@ -0,0 +1,47 @@
+import { JSONFile, Low } from "@commonify/lowdb";
+
+export class LowDB<T> implements Low<T> {
+	data!: T;
+	write!: Low<T>["write"];
+	read!: Low<T>["read"];
+	adapter!: Low<T>["adapter"];
+	private low: Low<T>;
+	private initialized: boolean;
+	waitInit() {
+		return new Promise<void>((res) => {
+			if (this.initialized) res();
+			const interval = setInterval(() => {
+				if (this.initialized) {
+					clearInterval(interval);
+					res();
+				}
+			}, 100);
+		});
+	}
+	constructor(filename: string, initialIfNull: T, onRead?: (data: T) => void) {
+		this.initialized = false;
+		this.low = new Low(new JSONFile<T>(filename));
+		this["data"] = this.low["data"] as T;
+		this["adapter"] = this.low["adapter"];
+		this["write"] = this.low["write"];
+		this["read"] = this.low["read"];
+
+		this.write().then(async () => {
+			let error: string | undefined;
+			try {
+				await this.read();
+			} catch (err) {
+				error = err;
+				console.log(
+					`File ${filename} =>`,
+					"Something went wrong while trying to read the file, resetting!"
+				); //TODO: maybe make a backup first
+			} finally {
+				this.data ||= initialIfNull;
+				this.initialized = true;
+				onRead?.(this.data);
+				if (error) await this.write();
+			}
+		});
+	}
+}
